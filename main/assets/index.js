51780ef2ebeecb546cbfcad27043a3f304344a4e
NoFox <nfox598625@gmail.com>
2023-10-23 18:46:47 -0400

    added license functions to generateMarkdown

--- main/assets/index.js
+++ main/assets/index.js
@@ -1,8 +1,73 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What's you GitHub username?",
+  },
 
+  {
+    type: "input",
+    name: "email",
+    message: "What's your email?",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "What's the name of your project?",
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project:",
+  },
+
+  {
+    type: "input",
+    name: "github",
+    message: "What's you GitHub username?",
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "What license does our project have?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "GLP3.0", "BSD2", "BSD3", "None"],
+  },
+
+  {
+    type: "input",
+    name: "dependencies",
+    message: "Any dependencies to install?",
+    default: "npm i",
+  },
+
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the proper usage of this repo?",
+  },
+
+  {
+    type: "input",
+    name: "contributors",
+    message: "Who are the contributors of this repo?",
+  },
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
--- main/assets/utils/generateMarkdown.js
+++ main/assets/utils/generateMarkdown.js
@@ -1,14 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![License](https://img.shields.io/nadge/License-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") return `*[License] (#License-📛)`;
+}
+return "";
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `
+    ## License 📛
+    Copyright @ ${license}. All rights reserved.
+    
+    Licensed under the ${license} license`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
